6165375fecf6e8aab753a603f8d02177bf215583
Aksana Lissikh <LissikhA@iba>
2018-09-14 12:25:19 +0300

    Add solveEquation function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  const array = equation.split(' ');
+  const a = Number(array[0]),
+    b = (array[3] === '-') ? -Number(array[4]) : Number(array[4]),
+    c = (array[7] === '-') ? -Number(array[8]) : Number(array[8]);
+  const D = Math.pow(b, 2) - 4 * a * c;
+  const answer = [];
+  answer.push(Math.round((-b + Math.sqrt(D)) / (2 * a)));
+  answer.push(Math.round((-b - Math.sqrt(D)) / (2 * a)));
+  return answer.sort((a, b) => a - b);
 }
